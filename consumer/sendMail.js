412be4b21f6d5d43cfa8a23d49577de8f5fc1ef3
LacamJC <joaoramajo744@gmail.com>
2025-04-13 21:18:24 -0300

    Usuário: LacamJC| Data: 2025-04-13 20:21

--- consumer/sendMail.js
+++ consumer/sendMail.js
@@ -2,32 +2,37 @@
 require('dotenv').config(); // Adicione esta linha no topo
 const nodemailer = require('nodemailer');
 
-exports.sendEmail = (para) => {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS
 
-        }
-    });
-    // 2. Opções do e-mail
-    const mailOptions = {
-        from: 'Email automatico <'+process.env.EMAIL+'>',
-        to: para,
-        subject: 'Você fez login em nosso site !',
-        html: '<h1>Muito obrigado</h1><p>Suas informações não foram salvas, este é somente um projeto feito para estudos.</p>',
-    };
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
 
-    // 3. Envio
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Erro:', error);
-        } else {
-            console.log('E-mail enviado:', info.response);
+    }
+});
+// 2. Opções do e-mail
+
+
+
+
+exports.sendEmail = async (para) => { // Tornar async
+    try {
+        const mailOptions = {
+            from: 'PHP-Hopper <' + process.env.EMAIL + '>',
+            to: para,
+            subject: 'Você fez login em nosso site !',
+            html: '<h1>Muito obrigado</h1><p>Suas informações não foram salvas, este é somente um projeto feito para estudos.</p>',
+        };
+        const info = await transporter.sendMail(mailOptions);
+        if(info){
+            console.log('Email enviado com sucesso');
         }
-    });
+        return info;
+    } catch (error) {
+        console.error('Falha no envio:', error.message);
+        
+    }
 }
-
